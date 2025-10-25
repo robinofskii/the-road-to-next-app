4bd13d1d6e38b33a4a44a920b7465ed43ff9c73b
Robin van den Berg <rvandenberg@frontliners.nl>
2025-10-25 19:51:19 +0300

    feat: replace Links with Button components for navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
+
 import { homePath, ticketsPath } from "./paths";
 
 import "./globals.css";
@@ -32,27 +34,24 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <nav>
-          <ul
-            className="supports-backdrop-blur:bg-background/60: bg-background/95
-              background-blur fixed top-0 right-0 left-0 z-20 flex w-full
-              justify-between border-b px-5 py-2.5"
-          >
+          <ul className="supports-backdrop-blur:bg-background/60: bg-background/95 background-blur fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5">
             <li>
-              <Link href={homePath()} className="text-lg font-bold">
-                Home
-              </Link>
+              <Button asChild variant="outline">
+                <Link href={homePath()} className="text-lg font-bold">
+                  Home
+                </Link>
+              </Button>
             </li>
             <li>
-              <Link href={ticketsPath()} className="text-sm underline">
-                Tickets
-              </Link>
+              <Button asChild>
+                <Link href={ticketsPath()} className="text-sm">
+                  Tickets
+                </Link>
+              </Button>
             </li>
           </ul>
         </nav>
-        <main
-          className="bg-secondary/20 flex min-h-screen flex-1 flex-col
-            overflow-x-hidden overflow-y-auto px-8 py-24"
-        >
+        <main className="bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-x-hidden overflow-y-auto px-8 py-24">
           {children}
         </main>
       </body>
