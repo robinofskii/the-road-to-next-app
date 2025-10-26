60a4a9356e3d9b3a7bfa2948ce8cfa999411ffa0
Robin van den Berg <rvandenberg@frontliners.nl>
2025-10-26 22:02:15 +0200

    refactor: replace custom navigation with HeaderNav component in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
-import { LucideCrosshair, LucideTags } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
-
-import { homePath, ticketsPath } from "./paths";
+import { HeaderNav } from "@/components/header-nav";
 
 import "./globals.css";
 
@@ -34,26 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>
-          <ul className="supports-backdrop-blur:bg-background/60: bg-background/95 background-blur fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5">
-            <li>
-              <Button asChild variant="ghost">
-                <Link href={homePath()}>
-                  <LucideCrosshair />
-                  <h1 className="text-lg font-semibold">TicketHunter</h1>
-                </Link>
-              </Button>
-            </li>
-            <li>
-              <Button asChild>
-                <Link href={ticketsPath()}>
-                  <LucideTags />
-                  Tickets
-                </Link>
-              </Button>
-            </li>
-          </ul>
-        </nav>
+        <HeaderNav />
         <main className="bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-x-hidden overflow-y-auto px-8 py-24">
           {children}
         </main>
