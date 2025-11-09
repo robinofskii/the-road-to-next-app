66a04adba5fcc2288f19030ae7b4a3b9e9d055e1
Robin van den Berg <robinofski.development@gmail.com>
2025-11-09 09:37:06 +0100

    feat: implement TicketsPage with ticket listing and navigation

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,30 @@
+import Link from "next/link";
+
+import BackButton from "@/components/BackButton";
+import { initialTickets } from "@/data";
+
 const TicketsPage = () => {
-  return <h2 className="text-lg">Tickets!</h2>;
+  return (
+    <div>
+      <div className="flex gap-2 items-center">
+        <BackButton />
+        <h1 className="text-2xl font-bold mb-4">Tickets</h1>
+      </div>
+      <ul className="space-y-4 flex flex-col">
+        {initialTickets.map((ticket) => (
+          <Link key={ticket.id} href={`/tickets/${ticket.id}`}>
+            <li key={ticket.id} className="p-4 border rounded shadow">
+              <h2 className="text-xl font-semibold">
+                {ticket.title}{" "}
+                <span className="text-sm text-gray-400">{ticket.status}</span>
+              </h2>
+              <p className="text-gray-600">{ticket.description}</p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TicketsPage;
