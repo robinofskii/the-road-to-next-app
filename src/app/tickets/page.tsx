8bf7e9df833547371b7970e4def958b607b11139
Robin van den Berg <robinofski.development@gmail.com>
2025-11-09 09:37:07 +0100

    feat: update TicketsPage layout with header and improved styling

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-import BackButton from "@/components/BackButton";
 import { initialTickets, TicketStatus } from "@/data";
 
 import { ticketPath } from "../paths";
@@ -13,19 +12,26 @@ const TICKET_ICONS: Record<TicketStatus, string> = {
 
 const TicketsPage = () => {
   return (
-    <div>
-      <div className="flex items-center gap-2">
-        <BackButton />
-        <h1 className="mb-4 text-2xl font-bold">Tickets</h1>
-      </div>
-      <ul className="flex flex-col space-y-4">
+    <div className="flex flex-1 flex-col gap-y-8">
+      <header>
+        <h2 className="text-3xl font-bold tracking-tight">Tickets</h2>
+        <p className="text-muted-foreground text-sm">
+          Here are all your tickets in the system.
+        </p>
+      </header>
+      <ul className="flex flex-1 flex-col items-center space-y-4">
         {initialTickets.map((ticket) => (
-          <Link key={ticket.id} href={ticketPath(ticket.id)}>
-            <li key={ticket.id} className="rounded border p-4 shadow">
-              <h2 className="text-xl font-semibold">
-                {ticket.title} <span>{TICKET_ICONS[ticket.status]}</span>
-              </h2>
-              <p className="text-gray-600">{ticket.description}</p>
+          <Link
+            key={ticket.id}
+            href={ticketPath(ticket.id)}
+            className="w-full max-w-md"
+          >
+            <li className="rounded border border-slate-100 p-4 shadow">
+              <h3 className="truncate text-xl font-semibold">
+                {ticket.title}{" "}
+                <span title={ticket.status}>{TICKET_ICONS[ticket.status]}</span>
+              </h3>
+              <p className="truncate text-slate-500">{ticket.description}</p>
             </li>
           </Link>
         ))}
