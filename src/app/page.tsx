0aeb427601703f309bdb63931b336cf40e9ce6f3
Robin van den Berg <robinofski.development@gmail.com>
2025-11-09 09:37:07 +0100

    feat: restructure HomePage layout with header and improved styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,19 @@ import { ticketsPath } from "./paths";
 
 const HomePage = () => {
   return (
-    <>
-      <h2 className="text-lg">Welcome to The Road to Next App!</h2>
-      <Link href={ticketsPath()} className="text-blue-500 underline">
-        View Tickets
-      </Link>
-    </>
+    <div className="flex flex-1 flex-col gap-y-8">
+      <header>
+        <h2 className="text-3xl font-bold tracking-tight">Homepage</h2>
+        <p className="text-muted-foreground text-sm">
+          This is your home page. Click the link below to view the tickets.
+        </p>
+      </header>
+      <article className="flex flex-1 flex-col items-center">
+        <Link href={ticketsPath()} className="text-blue-500 underline">
+          View Tickets
+        </Link>
+      </article>
+    </div>
   );
 };
 
