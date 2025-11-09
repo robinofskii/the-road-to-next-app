a61d3cbe87cfb600bf3b05e2489e1c66f9c483bc
Robin van den Berg <robinofski.development@gmail.com>
2025-11-09 09:37:08 +0100

    feat: create TicketLayout component for ticket details

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,8 +1,3 @@
-import Link from "next/link";
-
-import { ticketsPath } from "@/app/paths";
-import { Placeholder } from "@/components/placeholder";
-import { Button } from "@/components/ui/button";
 import { initialTickets } from "@/data";
 
 type TicketDetailsProps = {
@@ -16,28 +11,10 @@ const TicketDetailPage = async ({ params }: TicketDetailsProps) => {
 
   const ticketData = initialTickets.find((ticket) => ticket.id === ticketId);
 
-  if (!ticketData) {
-    return (
-      <Placeholder
-        title="Ticket not found"
-        description="The requested ticket does not exist."
-        button={
-          <Button asChild variant="outline">
-            <Link href={ticketsPath()}>Back to tickets</Link>
-          </Button>
-        }
-      />
-    );
-  }
+  if (!ticketData) return null;
 
   return (
     <div>
-      <div className="flex items-center gap-2">
-        <h1 className="mb-4 text-2xl font-bold">{ticketData.title}</h1>
-      </div>
-      <p className="mb-2">
-        <span className="font-semibold">Status:</span> {ticketData.status}
-      </p>
       <p className="mb-4">{ticketData.description}</p>
     </div>
   );
