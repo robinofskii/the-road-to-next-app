065a4ae2804fd1d9bf80f783e61e56eb278aad7b
Robin van den Berg <robinofski.development@gmail.com>
2025-11-09 09:37:06 +0100

    feat: enhance TicketDetailPage to display ticket information and handle not found cases

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,13 +1,33 @@
+import BackButton from "@/components/BackButton";
+import { initialTickets } from "@/data";
+
 type TicketDetailsProps = {
   params: {
     ticketId: string;
   };
 };
 
-const TicketDetailPage = ({ params }: TicketDetailsProps) => {
-  const { ticketId } = params;
+const TicketDetailPage = async ({ params }: TicketDetailsProps) => {
+  const { ticketId } = await params;
 
-  return <h2 className="text-lg">Ticket: {ticketId}</h2>;
+  const ticketData = initialTickets.find((ticket) => ticket.id === ticketId);
+
+  if (!ticketData) {
+    return <h2 className="text-lg">Ticket not found</h2>;
+  }
+
+  return (
+    <div>
+      <div className="flex gap-2 items-center">
+        <BackButton />
+        <h1 className="text-2xl font-bold mb-4">{ticketData.title}</h1>
+      </div>
+      <p className="mb-2">
+        <span className="font-semibold">Status:</span> {ticketData.status}
+      </p>
+      <p className="mb-4">{ticketData.description}</p>
+    </div>
+  );
 };
 
 export default TicketDetailPage;
